6f47f240cbb3c2e6481791e6caeac50a2506c938
radeeyate <124477460+radeeyate@users.noreply.github.com>
2023-07-13 15:57:11 -0600

    add all the things

---
+++ README.md
@@ -0,0 +1,14 @@
+# the api
+
+yeah, I an API that I made because I was bored and wanted to do a thing.
+
+the docs are located at https://api.radi8.dev/docs
+
+features:
+
+* password/pin generator (quite random)
+* rainbow (`curl -N https://api.radi8.dev/rainbow`, run in terminal)
+* Kids are much more accepting of other people than adults are.
+* memory info of my server
+* fake facts
+* Uptime stuff
---
+++ main.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI, Response, Query
+from fastapi.responses import StreamingResponse
+import time
+import random
+import string
+import secrets
+import asyncio
+import psutil
+
+app = FastAPI(title="radi8's api", description="radi8's multi-tool api")
+startTime = time.time()
+
+
+@app.get("/")
+def root():
+    return {"status": "ok", "version": 0.4, "uptime": time.time() - startTime, "docs": "/docs"}
+
+@app.get("/meminfo")
+def meminfo():
+    memory = psutil.virtual_memory()
+    return {"total": memory.total, "available": memory.available, "used": memory.used}
+
+
+@app.get("/password")
+def generatePassword(
+    length: int = Query(8, gt=0, lt=256),
+    includeUppercase: bool = True,
+    includeLowercase: bool = True,
+    includeDigits: bool = True,
+    includePunctuation: bool = True,
+):
+    if not any([includeUppercase, includeLowercase, includeDigits, includePunctuation]):
+        return {"error": "at least one option must be enabled"}
+
+    chars = ""
+    password = ""
+
+    if includeUppercase:
+        chars += string.ascii_uppercase
+        password += secrets.choice(string.ascii_uppercase)
+    if includeLowercase:
+        chars += string.ascii_lowercase
+        password += secrets.choice(string.ascii_lowercase)
+    if includeDigits:
+        chars += string.digits
+        password += secrets.choice(string.digits)
+    if includePunctuation:
+        chars += string.punctuation
+        password += secrets.choice(string.punctuation)
+
+    remainingLength = length - len(password)
+    password += "".join(secrets.choice(chars) for _ in range(remainingLength))
+
+    passwordList = list(password)
+    secrets.SystemRandom().shuffle(passwordList)
+    shuffledPassword = "".join(passwordList)
+
+    return Response(content=shuffledPassword)
+
+@app.get("/pin")
+def generatePin(length: int = Query(4, gt=3, lt=7)):
+    digits = string.digits
+    pin = "".join(secrets.choice(digits) for _ in range(length))
+
+    return Response(content=pin)
+
+
+@app.get("/rainbow")
+async def rainbow():
+    rainbow_colors = [
+        "\033[38;5;196m",  # red
+        "\033[38;5;202m",  # orange
+        "\033[38;5;226m",  # yellow
+        "\033[38;5;118m",  # green
+        "\033[38;5;45m",   # blue
+        "\033[38;5;99m",   # indigo
+        "\033[38;5;201m",  # violet
+    ]
+    delay = 0.5
+
+    async def generate_rainbow():
+        for _ in range(1):
+            for i in range(len(rainbow_colors)):
+                color_index = i % len(rainbow_colors)
+                color = rainbow_colors[color_index]
+                yield color + "███████████\033[0m\n"
+                await asyncio.sleep(delay)
+
+    return StreamingResponse(generate_rainbow(), media_type="text/plain")
+
+
+@app.get("/fakefact")
+def fakefact():
+    facts = (
+        "Salty water boils quicker",
+        "You swallow on average 8 spiders every year",
+        "If you drop a penny for the Empire State you can kill someone",
+        "Mt. Everest is the tallest mountain in the world",
+        "The world is flat",
+        "Blood without oxygen is blue",
+        "Tomatoes are vegatables",
+        "Humans only use 10% of their brains",
+        "Coffee stunts your growth",
+        "Coffee helps you sober up",
+        "Bananas are actually an herb",
+        "Zebras are just horses with stripes",
+        "The Loch Ness Monster is real",
+        "Napoleon Bonaparte was extremely short",
+        "Microwaving food removes its nutrients",
+        "The Moon landng was faked"
+    )
+    return Response(content = secrets.choice(facts))
+
+@app.get("/kidsaremuchmoreacceptingofotherpeoplethanadultsare")
+def kidsAreMuchMore():
+    return Response(content="Kids are much more accepting of other people than adults are.")
