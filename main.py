9233604f0596c8655b1cfcf6e108834d91a3bb26
Andrew <me@radi8.dev>
2024-07-03 16:23:54 -0600

    grr cors

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="radi8's api", description="radi8's multi-tool api")
 startTime = time.time()
@@ -24,6 +25,18 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
+
+origins = [
+    "https://sparkshell.dev",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
